27d2e06fd67c4f2624aff5e0b37291f48d6e7842
libraiger <misssilverbeauty0927@outlook.com>
2018-01-26 22:33:36 -0800

    Updated enemy combat and added ranged attacks to player

--- Assets/Scripts/Player Scripts/PlayerRangedAttack.cs
+++ Assets/Scripts/Player Scripts/PlayerRangedAttack.cs
@@ -6,22 +6,55 @@ public class PlayerRangedAttack : MonoBehaviour
 {
     public GameObject playerObject;
     public GameObject projectile;
+    public int playerDirection;
     public float throwForce;
+    private Vector2 daggerDirectionVector2;
+    private Vector3 daggerRotationVector3;
+
     // Use this for initialization
     void Start()
     {
         playerObject = GameObject.Find("Player");
-        throwForce = 275;
+        throwForce = 350;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerDirection = playerObject.GetComponent<PlayerController>().directionInt;
+        if (playerDirection == 0)
+        {
+            daggerRotationVector3 = -transform.forward;
+            daggerDirectionVector2 = new Vector2(0f, -throwForce);
+        }
+        if (playerDirection == 1)
+        {
+            daggerRotationVector3 = -transform.right;
+            daggerDirectionVector2 = new Vector2(-throwForce, 0f);
+        }
+        if (playerDirection == 2)
+        {
+            daggerRotationVector3 = -transform.right;
+            daggerDirectionVector2 = new Vector2(0f, throwForce);
+        }
+        if (playerDirection == 3)
+        {
+            daggerRotationVector3 = transform.right;
+            daggerDirectionVector2 = new Vector2(throwForce, 0f);
+        }
         if (Input.GetButtonDown("Throw"))
         {
+            Vector3 targetDir = daggerRotationVector3 - playerObject.transform.position;
+            // float angle = Mathf.Atan2(throwForce, throwForce) * Mathf.Rad2Deg - 90f; //-90f (for enemy direction)
+            //     Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            //     newKnife.transform.rotation = Quaternion.RotateTowards(daggerRotationVector3.transform.rotation, q, 90 * Time.deltaTime);
+
             GameObject newKnife = Instantiate(projectile, playerObject.transform.position, playerObject.transform.rotation);
-            newKnife.GetComponent<RangedDamage>().targetDir = new Vector2(90, 90);
-            newKnife.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(0f, throwForce));
+            newKnife.transform.Rotate (daggerDirectionVector2 * 90);
+            // Vector3 vForce = transform.forward * throwForce + transform.up * throwForce;
+            // newKnife.GetComponent<Rigidbody2D>().AddForce(vForce, ForceMode2D.Impulse);
+            // newKnife.transform.rotation = Quaternion.LookRotation(vForce);
+            newKnife.GetComponent<Rigidbody2D>().AddRelativeForce(-daggerDirectionVector2);
         }
     }
 }
--- Assets/Scripts/Player Scripts/PlayerRangedDamage.cs
+++ Assets/Scripts/Player Scripts/PlayerRangedDamage.cs
@@ -2,15 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRangedDamage : MonoBehaviour {
+public class PlayerRangedDamage : MonoBehaviour
+{
+    private EngagedWithPlayer playerEngagement;
+    private GameObject thisKnife;
+    private SFXManager sfxMan;
+    public GameObject swordClash;
+    public Transform swordClashPoint;
+    public Transform hitPoint;
+    public GameObject enemyObject;
+    public Vector3 targetDir;
+    public bool rangedDeathStrike;
+    private PlayerController thePlayer;
+    public int knifeDirection;
+    private ShieldBlock playerShield;
+    private float distanceToPlayer;
+    private GameObject playerObject;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        playerEngagement = FindObjectOfType<EngagedWithPlayer>();
+        thePlayer = FindObjectOfType<PlayerController>();
+        playerObject = GameObject.Find("Player");
+        playerShield = thePlayer.GetComponentInChildren<ShieldBlock>();
+        thisKnife = this.gameObject;
+        sfxMan = FindObjectOfType<SFXManager>();
+        rangedDeathStrike = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculates whether or not knife can hit player based on players shield and position relative to knife
+
+        //Quadrant 1
+        // if (targetDir.x < 0 && targetDir.y > 0)
+        // {
+        //     if (Mathf.Abs(targetDir.x) > targetDir.y)
+        //     {
+        //         knifeDirection = 3;
+        //     }
+        //     else if (Mathf.Abs(targetDir.x) < targetDir.y)
+        //     {
+        //         knifeDirection = 0;
+        //     }
+        // }
+        // //Quadrant 2
+        // else if (targetDir.x > 0 && targetDir.y > 0)
+        // {
+        //     if (targetDir.x < targetDir.y)
+        //     {
+        //         knifeDirection = 0;
+        //     }
+        //     else if (targetDir.x > targetDir.y)
+        //     {
+        //         knifeDirection = 1;
+        //     }
+        // }
+        // //Quardrant 3
+        // else if (targetDir.x > 0 && targetDir.y < 0)
+        // {
+        //     if (targetDir.x > Mathf.Abs(targetDir.y))
+        //     {
+        //         knifeDirection = 1;
+        //     }
+        //     else if (targetDir.x < Mathf.Abs(targetDir.y))
+        //     {
+        //         knifeDirection = 2;
+        //     }
+        // }
+        // //Quadrant 4
+        // else if (targetDir.x < 0 && targetDir.y < 0)
+        // {
+        //     if (targetDir.x < targetDir.y)
+        //     {
+        //         knifeDirection = 3;
+        //     }
+        //     else if (targetDir.x > targetDir.y)
+        //     {
+        //         knifeDirection = 2;
+        //     }
+        // }
+
+        // if (playerShield.shieldOn)
+        // {
+        //     if (knifeDirection - thePlayer.directionInt == -2
+        //         || knifeDirection - thePlayer.directionInt == 2)
+        //     {
+        //         rangedDeathStrike = false;
+        //     }
+        //     else
+        //     {
+        //         rangedDeathStrike = true;
+        //     }
+        // }
+        // else
+        // {
+        //     rangedDeathStrike = true;
+        // }
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "BasicRangedEnemy")
+        {
+            playerEngagement.doingDamage(1, thisKnife);
+        }
+        else if (other.gameObject.tag == "Wall")
+        {
+            distanceToPlayer = Vector3.Distance(transform.position, playerObject.transform.position);
+            if (distanceToPlayer < 12)
+            {
+                sfxMan.swordsColliding.volume = ((distanceToPlayer - 12) / -12) * ((distanceToPlayer - 12) / -12) - .1f;
+                sfxMan.swordsColliding.Play();
+            }
+            Instantiate(swordClash, hitPoint.position, hitPoint.rotation);
+            thisKnife.SetActive(false);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "BasicRangedEnemy")
+        {
+            thisKnife.SetActive(false);
+        }
+    }
 }
