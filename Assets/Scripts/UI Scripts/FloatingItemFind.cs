0cf5c50043118da0c800fe41468ac2dd7568fa61
libraiger <misssilverbeauty0927@outlook.com>
2018-02-25 15:18:18 -0800

    Added item drop descriptions on pickup

---
+++ Assets/Scripts/UI Scripts/FloatingItemFind.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FloatingItemFind : MonoBehaviour
+{
+    public float moveSpeed;
+    public int daggerCount;
+	public string itemType;
+    public Text displayNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayNumber.text = "+" + daggerCount + itemType;
+        transform.position = new Vector3(transform.position.x,
+            transform.position.y + (moveSpeed * Time.deltaTime), transform.position.z);
+    }
+}
--- Assets/Scripts/World Scripts/ItemDrop.cs
+++ Assets/Scripts/World Scripts/ItemDrop.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ItemDrop : MonoBehaviour
 {
     public string itemType;
+    public GameObject itemDesc;
+    private GameObject playerObject;
 
     void Start()
     {
         itemType = this.tag;
+        playerObject = GameObject.Find("Player");
     }
     public void GetItem()
     {
@@ -16,22 +19,38 @@ public class ItemDrop : MonoBehaviour
         {
             int randomNum = UnityEngine.Random.Range(1, 6);
             FindObjectOfType<PlayerRangedAttack>().daggerCount += randomNum;
+            var clone = (GameObject)Instantiate(itemDesc, playerObject.transform.position,
+                Quaternion.Euler(Vector3.zero));
+            if (randomNum > 1)
+            {
+                clone.GetComponent<FloatingItemFind>().itemType = " daggers";
+            }
+            else
+            {
+                clone.GetComponent<FloatingItemFind>().itemType = " dagger";
+            }
+            clone.GetComponent<FloatingItemFind>().daggerCount = randomNum;
         }
-        else if(itemType == "potion"){
+        else if (itemType == "potion")
+        {
             FindObjectOfType<PlayerHealthManager>().playerCurrentHealth += 3;
+            var clone = (GameObject)Instantiate(itemDesc, playerObject.transform.position,
+                Quaternion.Euler(Vector3.zero));
+            clone.GetComponent<FloatingItemFind>().itemType = " health";
+            clone.GetComponent<FloatingItemFind>().daggerCount = 3;
         }
     }
 
     public void CreateItem(GameObject enemyObject)
     {
-        int randomNum = UnityEngine.Random.Range(0, 31);
-        if (enemyObject.tag == "BasicRangedEnemy" && randomNum > 5)
+        int randomNum = UnityEngine.Random.Range(0, 51);
+        if (enemyObject.tag == "BasicRangedEnemy" && randomNum > 10)
         {
             GameObject randomDrop = GameObject.Find("Item_Pouch");
             GameObject newItem = Instantiate(randomDrop, enemyObject.transform.position, enemyObject.transform.rotation)
             ;
         }
-        else if (randomNum == 30)
+        else if (randomNum == 50)
         {
             GameObject randomDrop = GameObject.Find("Potion");
             GameObject newItem = Instantiate(randomDrop, enemyObject.transform.position, enemyObject.transform.rotation)
