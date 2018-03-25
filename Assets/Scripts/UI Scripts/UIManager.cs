58421a2b92d81afbea4833aad8ceab9513fdb824
libraiger <misssilverbeauty0927@outlook.com>
2018-03-25 14:31:44 -0700

    Level Up system update with blocking

--- Assets/Scripts/UI Scripts/UIManager.cs
+++ Assets/Scripts/UI Scripts/UIManager.cs
@@ -11,11 +11,16 @@ public class UIManager : MonoBehaviour
     public PlayerHealthManager playerHealth;
     public PlayerStaminaManager playerStamina;
     private PlayerRangedAttack playerRanged;
+    private ShieldBlock shieldBlockScript;
+    private PlayerStats playerStatsScript;
     private static bool UIExists;
     public GameObject daggerImageObject;
     public PlayerStats thePS;
+    private GameObject daggerTextObject;
+    private GameObject shieldTextObject;
+    private GameObject shieldImageObject;
     public Text daggerText;
-
+    public Text shieldText;
     public GameObject playerHealthObject;
 
     // Use this for initialization
@@ -25,6 +30,15 @@ public class UIManager : MonoBehaviour
         playerHealthObject = GameObject.Find("Player");
         playerHealth = playerHealthObject.GetComponent<PlayerHealthManager>();
         playerRanged = playerHealthObject.GetComponent<PlayerRangedAttack>();
+        shieldBlockScript = FindObjectOfType<ShieldBlock>();
+        playerStatsScript = FindObjectOfType<PlayerStats>();
+
+        daggerTextObject = GameObject.Find("DaggerText");
+        shieldTextObject = GameObject.Find("ShieldText");
+        shieldImageObject = GameObject.Find("ShieldImage");
+
+        shieldTextObject.SetActive(false);
+        shieldImageObject.SetActive(false);
 
         // daggerImageObject.GetComponent<Image>().Sprite = YourSprite;
 
@@ -45,12 +59,24 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (shieldBlockScript.shieldOn)
+        {
+            shieldImageObject.SetActive(true);
+            shieldTextObject.SetActive(true);
+        }
+        else
+        {
+            shieldImageObject.SetActive(false);
+            shieldTextObject.SetActive(false);
+        }
+
         healthbar.maxValue = playerHealth.playerMaxHealth;
         healthbar.value = playerHealth.playerCurrentHealth;
         HPText.text = "HP: " + playerHealth.playerCurrentHealth +
             "/" + playerHealth.playerMaxHealth;
 
         daggerText.text = ": " + playerRanged.daggerCount;
+        shieldText.text = ": " + shieldBlockScript.shieldBlocksLeft;
 
         staminaBar.maxValue = playerStamina.playerMaxStamina;
         staminaBar.value = playerStamina.playerCurrentStamina;
