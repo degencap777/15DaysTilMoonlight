f3620d764be21fb7999eb37b63ce989af1cd53d3
libraiger <misssilverbeauty0927@outlook.com>
2018-04-22 16:35:31 -0700

    item drop calculations

--- Assets/Scripts/World Scripts/ItemDrop.cs
+++ Assets/Scripts/World Scripts/ItemDrop.cs
@@ -47,19 +47,87 @@ public class ItemDrop : MonoBehaviour
 
     public void CreateItem(GameObject enemyObject)
     {
-        int randomNum = (UnityEngine.Random.Range(0, 51)) + ((playerStatScript.intelligence * 2)- 10);
-        if (enemyObject.tag == "BasicRangedEnemy" && randomNum > 10)
+        // eventually add in method to do random calculations
+        int randomNum = LuckCalculator();
+        Debug.Log("Luck: " + randomNum);
+        if (enemyObject.tag == "BasicRangedEnemy" && randomNum > 20)
         {
             GameObject randomDrop = GameObject.Find("daggers");
             GameObject newItem = Instantiate(randomDrop, enemyObject.transform.position, enemyObject.transform.rotation)
             ;
         }
-        if (randomNum >= 50)
+        if (randomNum >= 90)
         {
             GameObject randomDrop = GameObject.Find("potion");
-            GameObject newItem = Instantiate(randomDrop, enemyObject.transform.position, enemyObject.transform.rotation)
-            ;
+            GameObject newItem = Instantiate(randomDrop, enemyObject.transform.position, enemyObject.transform.rotation);
         }
+
+    }
+
+    public int LuckCalculator()
+    {
+        int randomNum = (UnityEngine.Random.Range(0, 101)) + ((playerStatScript.intelligence) - 5);
+        // tier 2
+        if (randomNum >= 100)
+        {
+            if (playerStatScript.intelligence >= 10)
+            {
+                randomNum = (UnityEngine.Random.Range(101, 201)) + ((playerStatScript.intelligence) - 5);
+            }
+            else
+            {
+                randomNum = (UnityEngine.Random.Range(101, 201));
+            }
+            // tier 3
+            if (randomNum >= 200)
+            {
+                if (playerStatScript.intelligence >= 20)
+                {
+                    randomNum = (UnityEngine.Random.Range(201, 301)) + ((playerStatScript.intelligence) - 5);
+                }
+                else
+                {
+                    randomNum = (UnityEngine.Random.Range(201, 301));
+                }
+                // tier 4
+                if (randomNum >= 300)
+                {
+                    if (playerStatScript.intelligence >= 30)
+                    {
+                        randomNum = (UnityEngine.Random.Range(301, 401)) + ((playerStatScript.intelligence) - 5);
+                    }
+                    else
+                    {
+                        randomNum = (UnityEngine.Random.Range(301, 401));
+                    }
+                }
+                // tier 5
+                if (randomNum >= 400)
+                {
+                    if (playerStatScript.intelligence >= 40)
+                    {
+                        randomNum = (UnityEngine.Random.Range(401, 501)) + ((playerStatScript.intelligence) - 5);
+                    }
+                    else
+                    {
+                        randomNum = (UnityEngine.Random.Range(401, 501));
+                    }
+                }
+                // tier 6
+                if (randomNum >= 500)
+                {
+                    if (playerStatScript.intelligence >= 50)
+                    {
+                        randomNum = (UnityEngine.Random.Range(501, 601)) + ((playerStatScript.intelligence) - 5);
+                    }
+                    else
+                    {
+                        randomNum = (UnityEngine.Random.Range(501, 601));
+                    }
+                }
+            }
+        }
+        return randomNum;
     }
 
     void OnTriggerEnter2D(Collider2D other)
